5c764ee0c69aa8967f4670cd9c76cebe6477cbb0
nbaybara <nsbaybar@gmail.com>
2020-09-18 03:41:17 +0300

    removed all files....i gitignore

--- spring-demo-1/src/com/nur/springdemo/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.nur.springdemo;
-
-public interface Coach {
-	public String getDailyWork();
-	public String getDailyFortune();
-	}
--- spring-demo-1/src/com/nur/springdemo/FortuneService.java
+++
@@ -1,7 +0,0 @@
-package com.nur.springdemo;
-
-public interface FortuneService {
-
-	public String getFortune();
-	public String getDailyFortune();
-}
